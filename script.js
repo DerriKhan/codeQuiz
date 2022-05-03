32228c72ec3e129c94d4a2037296d3034f7c7fbf
DerriKhan <101285294+DerriKhan@users.noreply.github.com>
2022-05-02 21:01:23 -0700

    added quiz structer

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+var question = document.getElementById('question');
+var answer = document.getElementById('answer');
 var timerEl = document.getElementById('timer');
+
 timerEl.addEventListener('click', timer);
 
 function timer() {
@@ -14,18 +17,59 @@ function timer() {
     }, 1000);
 };
 
-var questions = [
-    "What is the primary purpose of JavaScript?",
-    "Which of the following is NOT a primitive data type in JavaScript?",
-    'The “===“ operator checks for what?',
+var questionsAndAnswers = [
+    {   question: 'What is the primary purpose of JavaScript?',
+        answers: {
+        a: 'Generating styled elements for a webpage.',
+		b: 'Applying attributes to text elements.',
+		c: 'Providing interactivity for webpages.',
+		d: 'Creating bald developers.'
+        },
+        correctAnswer: 'c'
+    },
+    {   question: 'Which of the following is NOT a primitive data type in JavaScript?',
+        answers: {
+        a: 'Array',
+		B: 'Null',
+		c: 'Boolean',
+		d: 'Number'
+        },
+        correctAnswer: 'a'
+    },
+    {   question: 'The “===“ operator checks for what?',
+        answers: {
+        a: 'Equal data',
+		b: 'Equal data & type',
+		c: 'Equal type',
+		d: 'If you want to supersize for only $0.25'
+        },
+        correctAnswer: 'b'
+    },
+    {   question: 'DOM is an acronym for what?',
+        answers: {
+        a: 'Deliberate Obscure Method',
+		b: 'Descriptive Object Mention',
+		c: 'Downsize Obtuse Monoliths',
+		d: 'Document Object Model'
+        },
+        correctAnswer: 'd'
+    },
+    {   question: 'Where is an external javaScript file usually linked in HTML?',
+        answers: {
+        a: 'Inside the affected element tag',
+        b: 'Inside the <head> tag',
+        c: 'Within the <DOCTYPE> tag',
+        d: 'Javascript files are linked automatically'
+        },
+        correctAnswer: 'b'
+    },
 ]
 
-var answers = [
-        '1. Generating styled elements for a webpage.\n2. Applying attributes to text elements.\n3. Providing interactivity for webpages.\n4. Creating bald developers.'
-]
-var question = document.getElementById('question');
+question.append(questionsAndAnswers[1].question)
+answer.append(questionsAndAnswers[1].answers)
 
-question.append(questions);
-question.append(answers);
 
+
+
+console.log(questionsAndAnswers);
 console.log('linked!');
