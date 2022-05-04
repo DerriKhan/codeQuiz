c7d3fed94f00285140f7b5d5c9c4ab051e87f1f6
DerriKhan <101285294+DerriKhan@users.noreply.github.com>
2022-05-04 13:30:41 -0700

    completed quiz

--- script.js
+++ script.js
@@ -1,75 +1,106 @@
 var question = document.getElementById('question');
 var answer = document.getElementById('answer');
-var timerEl = document.getElementById('timer');
+var timer = document.getElementById('timer');
+var response = document.querySelector('button');
+var startingIndex = 0;
+var timeLeft = 60;
+var moreQuestion
+var questionsAndAnswers = [
+    {   question: 'What is the primary purpose of JavaScript?',
+        answers: [
+        "Generating styled elements for a webpage.",
+		"Applying attributes to text elements.",
+		"Providing interactivity for webpages.",
+		"Creating bald developers."
+    ],
+    correctAnswer: "Providing interactivity for webpages."
+},
+{   question: 'Which of the following is NOT a primitive data type in JavaScript?',
+answers: [
+    'Array',
+    'Null',
+    'Boolean',
+    'Number'
+],
+correctAnswer: 'Array'
+},
+{   question: 'The “===“ operator checks for what?',
+answers: [
+    'Equal data',
+    'Equal data & type',
+    'Equal type',
+    'If you want to supersize for only $0.25'
+],
+correctAnswer: 'Equal data & type'
+},
+{   question: 'DOM is an acronym for what?',
+answers: [
+    'Deliberate Obscure Method',
+    'Descriptive Object Mention',
+	'Downsize Obtuse Monoliths',
+	'Document Object Model'
+    ],
+    correctAnswer: 'Document Object Model'
+},
+{   question: 'Where is an external javaScript file usually linked in HTML?',
+answers: [
+    'Inside the affected element tag',
+    'Inside the <head> tag',
+    'Within the <DOCTYPE> tag',
+    'Javascript files are linked automatically'
+],
+correctAnswer: 'Inside the <head> tag'
+},
+]
 
-timerEl.addEventListener('click', timer);
-
-function timer() {
-    var timeLeft = 5;
-    var timeInterval = setInterval(function() {
+timer.addEventListener('click', countDown);
+function countDown() {
+    askQuestion()
+    timeLeft
+    setInterval(function() {
         if(timeLeft > 0) {
-        timerEl.textContent = timeLeft;
-        timeLeft--;
+            timer.textContent = timeLeft;
+            timeLeft--;
         } else {
-        timerEl.textContent = "Time is up!";
-        clearInterval(timeInterval);
+            timer.textContent = "Time is up!";
         }
     }, 1000);
 };
 
-var questionsAndAnswers = [
-    {   question: 'What is the primary purpose of JavaScript?',
-        answers: {
-        a: 'Generating styled elements for a webpage.',
-		b: 'Applying attributes to text elements.',
-		c: 'Providing interactivity for webpages.',
-		d: 'Creating bald developers.'
-        },
-        correctAnswer: 'c'
-    },
-    {   question: 'Which of the following is NOT a primitive data type in JavaScript?',
-        answers: {
-        a: 'Array',
-		B: 'Null',
-		c: 'Boolean',
-		d: 'Number'
-        },
-        correctAnswer: 'a'
-    },
-    {   question: 'The “===“ operator checks for what?',
-        answers: {
-        a: 'Equal data',
-		b: 'Equal data & type',
-		c: 'Equal type',
-		d: 'If you want to supersize for only $0.25'
-        },
-        correctAnswer: 'b'
-    },
-    {   question: 'DOM is an acronym for what?',
-        answers: {
-        a: 'Deliberate Obscure Method',
-		b: 'Descriptive Object Mention',
-		c: 'Downsize Obtuse Monoliths',
-		d: 'Document Object Model'
-        },
-        correctAnswer: 'd'
-    },
-    {   question: 'Where is an external javaScript file usually linked in HTML?',
-        answers: {
-        a: 'Inside the affected element tag',
-        b: 'Inside the <head> tag',
-        c: 'Within the <DOCTYPE> tag',
-        d: 'Javascript files are linked automatically'
-        },
-        correctAnswer: 'b'
-    },
-]
+askQuestion();
 
-question.append(questionsAndAnswers[1].question)
-answer.append(questionsAndAnswers[1].answers)
+function askQuestion() {
+    question.append(questionsAndAnswers[startingIndex].question);
+    questionsAndAnswers[startingIndex].answers.forEach(function(answers) {
+        var button = document.createElement('button')
+        button.textContent = answers;
+        answer.append(button)
+        button.addEventListener('click', (function() {
+            var choice = button.textContent;
+            if (choice === questionsAndAnswers[startingIndex].correctAnswer) {
+                console.log("correct");
+            } else {
+                timeLeft = timeLeft - 10;
+                 console.log("incorrect")
+            }
+            nextQuestion();
+        }));
+    });
+};
 
+function nextQuestion() {
+    startingIndex++
+    if(startingIndex < questionsAndAnswers.length) {
+        question.textContent = "";
+        answer.textContent = "";
+        askQuestion();
+    } else {
+        score();
+    }
+}
 
-
-
-console.log(questionsAndAnswers);
-console.log('linked!');
+function score(timeLeft) {
+    clearInterval(timeLeft);
+    console.log(timeLeft);
+    
+}
